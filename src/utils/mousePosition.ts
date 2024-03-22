e982707942c795681bbbe95078e89e9ce0e0cf03
한준희 <hanjunhui@hanjunhuiui-Macmini.local>
2024-03-22 16:59:49 +0900

    utils 테스트 240322

---
+++ src/utils/mousePosition.ts
@@ -0,0 +1,20 @@
+import { ref, onMounted, onUnmounted } from 'vue'
+
+export function useMouse() {
+  const x = ref(0)
+  const y = ref(0)
+
+  function update(event: { pageX: number; pageY: number }) {
+    x.value = event.pageX
+    y.value = event.pageY
+  }
+
+  onMounted(() => window.addEventListener('mousemove', update))
+  onUnmounted(() => window.removeEventListener('mousemove', update))
+
+  return { x, y }
+}
+
+// 외부 사용시 - Utils(composable)의 기능을 컴포넌트에서 사용할 시 
+// import { useMouse } from './mouse.js'
+// const { x, y } = useMouse()
