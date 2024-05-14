30fc9a6e9c85ea57d209f86679c53a903fb901eb
jnlee <jnlee@wooricnc.com>
2024-05-14 15:41:44 +0900

    about 페이지 작업

--- src/components/pages/about/index.vue
+++ src/components/pages/about/index.vue
@@ -4,11 +4,11 @@
       <div class="aboutVisual" :style="{height: heightGetter+'px' ? heightGetter+'px' : ''}">
         <img src="@/assets/images/img-about-visual.png" alt="루멘_배경이미지">
         <div>
-          <p><img class="logoImg" src="@/assets/images/logo.png" alt=""></p>
-          <strong class="ko">당신의 공간엔 당신의 가치를.</strong>
-          <p class="ko">루멘은 가구의 가치를 뛰어넘는 미디데스크 전문 브랜드입니다.<br/>고객님의 개성과 가치가 공간에도 고스란히 담길 수 있기를 바랍니다.</p>
-          <strong class="eng">Your value in your space.</strong>
-          <p class="eng">Roomen is a midi table brand that goes beyond the value of furniture.<br/> We hope that your personality and values ​​can be fully reflected in your space.</p>
+          <p class="aniOne"><img class="logoImg" src="@/assets/images/logo.png" alt=""></p>
+          <strong class="ko aniTwo">당신의 공간엔 당신의 가치를.</strong>
+          <p class="ko aniThree">루멘은 가구의 가치를 뛰어넘는 미디데스크 전문 브랜드입니다.<br/>고객님의 개성과 가치가 공간에도 고스란히 담길 수 있기를 바랍니다.</p>
+          <strong class="eng aniFour">Your value in your space.</strong>
+          <p class="eng aniFive">Roomen is a midi table brand that goes beyond the value of furniture.<br/> We hope that your personality and values ​​can be fully reflected in your space.</p>
         </div>
       </div>
     </div>
