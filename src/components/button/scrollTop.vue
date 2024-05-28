d380ca14cb3566ecc964ff811a235f289c0832f6
jnlee <jnlee@wooricnc.com>
2024-05-28 14:20:15 +0900

    20240528_스크롤탑 기능수정

--- src/components/button/scrollTop.vue
+++ src/components/button/scrollTop.vue
@@ -2,23 +2,11 @@
     <button type="button" class="scrollTop" @click="scrollToTop"><span class="hide">위로 이동</span></button>
   </template>
   <script setup lang="ts">
-    import { ref, onMounted, onUnmounted } from 'vue';
+    import {ref} from 'vue';
 
     const scrollTop = ref(0);
 
     const scrollToTop = () => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     };
-
-    const onScroll = () => {
-        scrollTop.value = window.scrollY;
-    };
-
-    onMounted(() => {
-        window.addEventListener('scroll', onScroll);
-    });
-
-    onUnmounted(() => {
-        window.removeEventListener('scroll', onScroll);
-    });
   </script>
