49afc1686a4a9683fa4a325c51b4f8fe80c36182
jnlee <jnlee@wooricnc.com>
2024-05-28 14:22:58 +0900

    20240528_스크롤탑 수정(최종)

--- src/components/button/scrollTop.vue
+++ src/components/button/scrollTop.vue
@@ -2,10 +2,6 @@
     <button type="button" class="scrollTop" @click="scrollToTop"><span class="hide">위로 이동</span></button>
   </template>
   <script setup lang="ts">
-    import {ref} from 'vue';
-
-    const scrollTop = ref(0);
-
     const scrollToTop = () => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     };
