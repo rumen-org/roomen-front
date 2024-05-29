fa79aa47cc68aa91682808e86f53d15b1bdab373
bassistjune <bassistjune@gmail.com>
2024-05-29 16:04:43 +0900

    240529_product/mididesk 수정

--- src/components/pages/product/midiDesk/list.vue
+++ src/components/pages/product/midiDesk/list.vue
@@ -22,28 +22,19 @@
         <!--// conTopArea -->
         <!-- productList -->
         <div class="productList">
-          <router-link :to="`midiDesk/${item[0].id}`" class="product">
-            <p><img src="@/assets/images/img-product-4.png" alt=""></p>
-            <div>
-              <strong class="tit">루멘 미디데스크</strong>
-              <p class="subTit">Roomen MIDI DESK</p>
-              <p class="price">
-                <span>₩ 1,350,000</span>
-                <del>₩ 1,550,000</del>
-              </p>
-            </div>
-          </router-link>
-          <a href="" class="product">
-            <p><img src="@/assets/images/img-product-2.png" alt=""></p>
-            <div>
-              <strong class="tit">루멘 미디데스크</strong>
-              <p class="subTit">Roomen MIDI DESK</p>
-              <p class="price">
-                <span>₩ 1,350,000</span>
-                <del>₩ 1,550,000</del>
-              </p>
-            </div>
-          </a>
+          <template v-for="(item, index) in itemBox" :key="index">
+            <router-link :to="`midiDesk/${item.id}`" class="product">
+              <p><img :src="`src${item.ThumbImg}`" :alt="item.ThumbAlt"></p>
+              <div>
+                <strong class="tit">{{ item.title }}</strong>
+                <p class="subTit">{{ item.subTit }}</p>
+                <p class="price">
+                  <span>{{ item.discount }}</span>
+                  <del>{{ item.price }}</del>
+                </p>
+              </div>
+            </router-link>
+          </template>
         </div>
         <!--// productList -->
         <!-- bottomArea -->
@@ -67,10 +58,11 @@
   </template>
   
   <script setup lang="ts">
-  const item = [
-      {id: "id1", contents: "text..."},
-      {id: "id2", contents: "text..."},
-      {id: "id3", contents: "text..."}
+  const itemBox = [
+      {id: "id1",title: "루멘 미디데스크1" ,subTit: "Roomen MIDI DESK1", price: "1,550,001", discount: "1,350,001" , contents: "text...",ThumbImg: "/assets/images/img-product-4.png", ThumbAlt: "상품_섬네일1"},
+      {id: "id2",title: "루멘 미디데스크2" ,subTit: "Roomen MIDI DESK2", price: "1,550,002", discount: "1,350,002" , contents: "text...",ThumbImg: "/assets/images/img-product-2.png", ThumbAlt: "상품_섬네일2"},
+      {id: "id3",title: "루멘 미디데스크3" ,subTit: "Roomen MIDI DESK3", price: "1,550,003", discount: "1,350,003" , contents: "text...",ThumbImg: "/assets/images/img-product-4.png", ThumbAlt: "상품_섬네일3"},
+
   ]
   </script>
   
