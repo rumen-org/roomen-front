6ec47b79f4184413652bc791c5decd5ddcd579bc
bassistjune <bassistjune@gmail.com>
2024-05-29 16:59:38 +0900

    240529_product/mididesk 수정

--- src/components/pages/product/midiDesk/[item].vue
+++ src/components/pages/product/midiDesk/[item].vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="container">
+    <div id="container" @scroll="scrolling">
         <div class="contents">
             <!-- conTopArea -->
             <div class="conTopArea">
@@ -15,27 +15,28 @@
                     <!-- imgList -->
                     <div class="imgList">
                         <div class="preList">
-                            <a href=""><img src="../assets/images/img-product-4.png" alt=""></a>
-                            <a href=""><img src="../assets/images/img-product.jpg" alt=""></a>
-                            <a href=""><img src="../assets/images/img-product-4.png" alt=""></a>
-                            <a href=""><img src="../assets/images/img-product.png" alt=""></a>
-                            <a href=""><img src="../assets/images/img-product-5.png" alt=""></a>
+                            <a href=""><img src="@/assets/images/img-product-4.png" alt=""></a>
+                            <a href=""><img src="@/assets/images/img-product.jpg" alt=""></a>
+                            <a href=""><img src="@/assets/images/img-product-4.png" alt=""></a>
+                            <a href=""><img src="@/assets/images/img-product.png" alt=""></a>
+                            <a href=""><img src="@/assets/images/img-product-5.png" alt=""></a>
                         </div>
                         <div class="preArea">
-                            <img src="../assets/images/img-product-4.png" alt="">
+                            <img src="@/assets/images/img-product-4.png" alt="">
                         </div>
                     </div>
                     <!--// imgList -->
 
                     <!-- floatMenu -->
-                    <div class="floatMenu">
-                        <strong class="tit">루멘 미디데스크</strong>
-                        <p class="subTit fontG">Roomen MIDI DESK</p>
-                        <p class="price">
+                    <div class="floatMenu" ref="flottingMenu" >
+                        <div class="floatScroll">
+                          <strong class="tit">루멘 미디데스크</strong>
+                          <p class="subTit fontG">Roomen MIDI DESK</p>
+                          <p class="price">
                             <span>₩ 1,350,000</span>
                             <del>₩ 1,550,000</del>
-                        </p>
-                        <div class="option">
+                          </p>
+                          <div class="option">
                             <p>색상1 : 상판, 하부다리, 후면가림판</p>
                             <!-- checkboxWrap -->
                             <div class="checkboxWrap">
@@ -43,46 +44,46 @@
                                     <input type="radio" id="chk0101" name="chk01">
                                     <label for="chk0101">화이트</label>
                                 </span>
-                                <span class="checkbox noTxt black">
+                              <span class="checkbox noTxt black">
                                     <input type="radio" id="chk0102" name="chk01">
                                     <label for="chk0102">블랙</label>
                                 </span>
-                                <span class="checkbox noTxt gray">
+                              <span class="checkbox noTxt gray">
                                     <input type="radio" id="chk0103" name="chk01">
                                     <label for="chk0103">그레이</label>
                                 </span>
-                                <span class="checkbox noTxt brown">
+                              <span class="checkbox noTxt brown">
                                     <input type="radio" id="chk0104" name="chk01">
                                     <label for="chk0104">브라운</label>
                                 </span>
-                                <span class="checkbox noTxt beige">
+                              <span class="checkbox noTxt beige">
                                     <input type="radio" id="chk0105" name="chk01">
                                     <label for="chk0105">베이지</label>
                                 </span>
-                                <span class="checkbox noTxt red">
+                              <span class="checkbox noTxt red">
                                     <input type="radio" id="chk0106" name="chk01">
                                     <label for="chk0106">레드</label>
                                 </span>
-                                <span class="checkbox noTxt blue">
+                              <span class="checkbox noTxt blue">
                                     <input type="radio" id="chk0107" name="chk01">
                                     <label for="chk0107">블루</label>
                                 </span>
-                                <span class="checkbox noTxt yellow">
+                              <span class="checkbox noTxt yellow">
                                     <input type="radio" id="chk0108" name="chk01" checked>
                                     <label for="chk0108">옐로루</label>
                                 </span>
-                                <span class="checkbox noTxt green">
+                              <span class="checkbox noTxt green">
                                     <input type="radio" id="chk0109" name="chk01">
                                     <label for="chk0109">그린</label>
                                 </span>
-                                <span class="checkbox noTxt purple">
+                              <span class="checkbox noTxt purple">
                                     <input type="radio" id="chk0110" name="chk01">
                                     <label for="chk0110">퍼플</label>
                                 </span>
                             </div>
                             <!--// checkboxWrap -->
-                        </div>
-                        <div class="option">
+                          </div>
+                          <div class="option">
                             <p>색상2 : 건반트레이, 다리, 상단선반, 칸막이</p>
                             <!-- checkboxWrap -->
                             <div class="checkboxWrap">
@@ -90,85 +91,86 @@
                                     <input type="radio" id="chk0201" name="chk02" checked>
                                     <label for="chk0201">화이트</label>
                                 </span>
-                                <span class="checkbox noTxt black">
+                              <span class="checkbox noTxt black">
                                     <input type="radio" id="chk0202" name="chk02">
                                     <label for="chk0202">블랙</label>
                                 </span>
-                                <span class="checkbox noTxt gray">
+                              <span class="checkbox noTxt gray">
                                     <input type="radio" id="chk0203" name="chk02">
                                     <label for="chk0203">그레이</label>
                                 </span>
-                                <span class="checkbox noTxt brown">
+                              <span class="checkbox noTxt brown">
                                     <input type="radio" id="chk0204" name="chk02">
                                     <label for="chk0204">브라운</label>
                                 </span>
-                                <span class="checkbox noTxt beige">
+                              <span class="checkbox noTxt beige">
                                     <input type="radio" id="chk0205" name="chk02">
                                     <label for="chk0205">베이지</label>
                                 </span>
-                                <span class="checkbox noTxt red">
+                              <span class="checkbox noTxt red">
                                     <input type="radio" id="chk0206" name="chk02">
                                     <label for="chk0206">레드</label>
                                 </span>
-                                <span class="checkbox noTxt blue">
+                              <span class="checkbox noTxt blue">
                                     <input type="radio" id="chk0207" name="chk02">
                                     <label for="chk0207">블루</label>
                                 </span>
-                                <span class="checkbox noTxt yellow">
+                              <span class="checkbox noTxt yellow">
                                     <input type="radio" id="chk0208" name="chk02">
                                     <label for="chk0208">옐로우</label>
                                 </span>
-                                <span class="checkbox noTxt green">
+                              <span class="checkbox noTxt green">
                                     <input type="radio" id="chk0209" name="chk02">
                                     <label for="chk0209">그린</label>
                                 </span>
-                                <span class="checkbox noTxt purple">
+                              <span class="checkbox noTxt purple">
                                     <input type="radio" id="chk0210" name="chk02">
                                     <label for="chk0210">퍼플</label>
                                 </span>
                             </div>
                             <!--// checkboxWrap -->
-                        </div>
-                        <dl>
+                          </div>
+                          <dl>
                             <dt>배송비</dt>
                             <dd>
-                                <select title="배송비">
-                                    <option>[필수] 주문시 결제</option>
-                                </select>
+                              <select title="배송비">
+                                <option>[필수] 주문시 결제</option>
+                              </select>
                             </dd>
-                        </dl>
-                        <dl>
+                          </dl>
+                          <dl>
                             <dt>추가옵션</dt>
                             <dd>
-                                <select title="추가옵션">
-                                    <option>[선택] 추가옵션</option>
-                                </select>
+                              <select title="추가옵션">
+                                <option>[선택] 추가옵션</option>
+                              </select>
                             </dd>
-                        </dl>
-                        <div class="totalArea">
+                          </dl>
+                          <div class="totalArea">
                             <p><span>루멘 미디데스크</span> - 옐로우/화이트 /수도권 (서울,경기,인천) (+ ₩50 ,000)/없음</p>
                             <div>
-                                <div class="count">
-                                    <div>
-                                        <button type="button" class="minusBtn"><span class="hide">빼기</span></button>
-                                        <span>1</span>
-                                        <button type="button" class="plusBtn"><span class="hide">더하기</span></button>
-                                    </div>
-                                    <button type="button"><span class="hide">옵션삭제</span></button>
+                              <div class="count">
+                                <div>
+                                  <button type="button" class="minusBtn"><span class="hide">빼기</span></button>
+                                  <span>1</span>
+                                  <button type="button" class="plusBtn"><span class="hide">더하기</span></button>
                                 </div>
+                                <button type="button"><span class="hide">옵션삭제</span></button>
+                              </div>
                             </div>
-                        </div>
-                        <div class="totalPrice">
+                          </div>
+                          <div class="totalPrice">
                             <span class="fontG">TOTAL</span>
                             <em>₩ 1,400,000</em>
-                        </div>
-                        <div class="btnArea">
+                          </div>
+                          <div class="btnArea">
                             <button type="button" class="btn sL">구매하기</button>
                             <button type="button" class="btn bgWhite cartBtn"><span class="hide">장바구니</span></button>
+                          </div>
                         </div>
                     </div>
                     <!--// floatMenu -->
-                    
+
                     <div class="detail">
                         <div style="background:#F8F8F8; min-height:1000px;">
                             상세정보 영역
@@ -197,5 +199,29 @@
     </div>
 </template>
 <script setup lang="ts">
+import {onMounted, onUnmounted, ref} from "vue"
+const flottingMenu = ref<HTMLElement | null>(null);
+const outterHeight = ref<number>(window.outerHeight);
+const scrolling = (e: Event) => {
+  outterHeight.value = window.outerHeight - flottingMenu.value?.offsetTop;
+  console.log('flottingMenu',flottingMenu.value?.offsetTop, flottingMenu.value?.clientTop,e)
+}
+onMounted(() => {
+  window.addEventListener('scroll', scrolling);
+});
 
+// 컴포넌트가 언마운트될 때 이벤트 리스너 제거
+onUnmounted(() => {
+  window.removeEventListener('scroll', scrolling);
+});
 </script>
+<style lang="scss">
+.floatScroll {position: absolute; left: 0; right: 0; top:0; bottom: 0; overflow-y: scroll; overflow-x: hidden;
+  background: #fff;
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+}
+.floatScroll::-webkit-scrollbar {
+  display: none; /* Chrome, Safari, Opera*/
+}
+</style>
