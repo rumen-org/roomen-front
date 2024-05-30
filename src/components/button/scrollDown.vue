753108d555762c433698b6ebffe97f0ca3714c40
jnlee <jnlee@wooricnc.com>
2024-05-30 16:28:11 +0900

    스크롤다운 스크립트 수정

--- src/components/button/scrollDown.vue
+++ src/components/button/scrollDown.vue
@@ -1,6 +1,20 @@
 <template>
-    <button type="button" class="scrollDown"><span class="hide">아래로 스크롤</span></button>
-  </template>
-  <script setup lang="ts">
+    <button type="button" class="scrollDown" v-if="showScrollDown"><span class="hide">아래로 스크롤</span></button>
+</template>
+<script setup lang="ts">
+    import { ref, onMounted, onBeforeUnmount } from 'vue';
 
-  </script>
+    const showScrollDown = ref(true);
+
+    const handleScroll = () => {
+        showScrollDown.value = window.scrollY === 0;
+    };
+
+    onMounted(() => {
+        window.addEventListener('scroll', handleScroll);
+    });
+
+    onBeforeUnmount(() => {
+        window.removeEventListener('scroll', handleScroll);
+    });
+</script>
