3e91da7fbaec1678c9d2cd8c84c3174ab946a3a6
bassistjune <bassistjune@gmail.com>
2025-07-16 16:30:04 +0900

    fix: 변경사항 적용 및 이미지 변경

--- src/components/pages/about/index.vue
+++ src/components/pages/about/index.vue
@@ -1,10 +1,7 @@
 <template>
   <div id="container">
     <div class="inner">
-      <div
-        class="about-visual"
-        :style="{ height: responseHeight === 0 ? '100%' : responseHeight + 'px' }"
-      >
+      <div class="about-visual">
         <!--        <img src="@/assets/images/img-about-visual.png" alt="루멘_배경이미지">-->
         <div>
           <p class="ani-one"><img class="logo-img" src="@/assets/images/logo.png" alt="" /></p>
@@ -26,9 +23,7 @@
 <script setup lang="ts">
 // import { debounce } from 'lodash'
 // import { onMounted, onUnmounted, ref } from 'vue'
-import { ref } from 'vue'
-
-const responseHeight = ref<number>(0)
+// const responseHeight = ref<number>(0)
 // const heightGetter = debounce(() => {
 //   responseHeight.value = window.innerHeight
 // }, 500)
